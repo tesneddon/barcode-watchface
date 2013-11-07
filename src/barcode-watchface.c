854dc442e580fec602a72e67f9f88bb04ef3d7d9
Tim E. Sneddon <tim@sneddon.id.au>
2013-11-07 12:55:55 +0800

    Updated .gitignore to leave out waf lock file. Updated appinfo.json with correctly translated uuid. Main source module now works to load/unload.  Now, start handling display of the time!

--- src/barcode-watchface.c
+++ src/barcode-watchface.c
@@ -51,190 +51,98 @@
 **					leading zeroes.
 **--
 */
-#include "pebble_os.h"
-#include "pebble_app.h"
-#include "pebble_fonts.h"
+#include <pebble.h>
+
 #define BAR_WIDTH 3
 
 /*
 ** Forward declarations
 */
 
-    void pbl_main(void *params);
-    void handle_init(AppContextRef ctx);
-    void handle_tick(AppContextRef ctx, PebbleTickEvent *event);
-    void update_time_layer(Layer *layer, GContext *gc);
-    void handle_deinit(AppContextRef ctx);
+    int main(void);
+    static void init(void);
+    static void deinit(void);
 
 /*
-** Pebble Watchapp Identification
+** Own storage
 */
 
-#define MY_UUID { 0xF7, 0x8F, 0xBD, 0xDE, \
-		  0x9A, 0x65, 0x40, 0x3A, \
-		  0x84, 0xB8, 0x54, 0xDE, \
-		  0x35, 0x6A, 0x10, 0x41 }
-
-PBL_APP_INFO(MY_UUID,
-             "Barcode Watchface", "Tim Sneddon",
-             1, 0, /* App version */
-             DEFAULT_MENU_ICON,
-             APP_INFO_WATCH_FACE);
+    static const int BAR_RESOURCE_ID[] = {
+    	RESOURCE_ID_0, RESOURCE_ID_1, RESOURCE_ID_2, RESOURCE_ID_3,
+    	RESOURCE_ID_4, RESOURCE_ID_5, RESOURCE_ID_6, RESOURCE_ID_7,
+	RESOURCE_ID_8, RESOURCE_ID_9,
+    };
+    static GBitmap *bar[10], *left, *right;
+    static BitmapLayer *bar_layer[6], *left_layer, *right_layer;
+    static Window *window;
 
 /*
 ** Global storage
 */
 
-    HeapBitmap left, bar[10], right;
-    BitmapLayer bar_layer[6], left_layer, right_layer;;
-    TextLayer text_layer;
-    Window window;
+static void init(void) {
 
-void pbl_main(void *params) {
-    PebbleAppHandlers handlers = {
-    	.init_handler = handle_init,
-	.tick_info = {
-	    .tick_handler = handle_tick,
-	    .tick_units = SECOND_UNIT | MINUTE_UNIT | HOUR_UNIT,
-	},
-	.deinit_handler = handle_deinit,
-    };
-    app_event_loop(params, &handlers);
-}
-
-void handle_up(AppContextRef app_ctx,
-	       Window *window,
-	       PebbleButtonEvent *event) {
-
-    layer_set_hidden(&right_layer.layer, true);
-
-}
-
-void handle_init(AppContextRef ctx) {
-
-    PebbleTickEvent event;
-    PblTm now;
+    Layer *window_layer;
     unsigned i;
 
-    window_init(&window, "Barcode Watchface");
-    window_stack_push(&window, true /* Animated */);
-    window_set_background_color(&window, GColorWhite);
-    window.overrides_back_button = true;
-    window.input_handlers.buttons.up = handle_up;
-    window.input_handlers.buttons.down = handle_up;
-
-    resource_init_current_app(&APP_RESOURCES);
-
     /*
-    ** Configure the time barcode layer.
+    ** Initialize the main window.
     */
-    heap_bitmap_init(&bar[0], RESOURCE_ID_0);
-    heap_bitmap_init(&bar[1], RESOURCE_ID_1);
-    heap_bitmap_init(&bar[2], RESOURCE_ID_2);
-    heap_bitmap_init(&bar[3], RESOURCE_ID_3);
-    heap_bitmap_init(&bar[4], RESOURCE_ID_4);
-    heap_bitmap_init(&bar[5], RESOURCE_ID_5);
-    heap_bitmap_init(&bar[6], RESOURCE_ID_6);
-    heap_bitmap_init(&bar[7], RESOURCE_ID_7);
-    heap_bitmap_init(&bar[8], RESOURCE_ID_8);
-    heap_bitmap_init(&bar[9], RESOURCE_ID_9);
-
-    heap_bitmap_init(&left, RESOURCE_ID_LEFT);
-    heap_bitmap_init(&right, RESOURCE_ID_RIGHT);
-
-    /*
-    ** Configure the layers...
-    */
-    bitmap_layer_init(&left_layer, GRect(21, 10, 6, 135));
-    bitmap_layer_set_bitmap(&left_layer, &left.bmp);
-    layer_add_child(&window.layer, &left_layer.layer);
-
-    for (i = 0; i < sizeof(bar_layer) / sizeof(bar_layer[0]); i++) {
-	bitmap_layer_init(&bar_layer[i],
-			  GRect(27 + (i * 14), 10, 14, 120));
-	layer_add_child(&window.layer, &bar_layer[i].layer);
-    }
-
-    bitmap_layer_init(&right_layer, GRect(111, 10, 12, 135));
-    bitmap_layer_set_bitmap(&right_layer, &right.bmp);
-    layer_add_child(&window.layer, &right_layer.layer);
-
-    text_layer_init(&text_layer, GRect(29, 134, 82, 21));
-    text_layer_set_font(&text_layer,
-			fonts_get_system_font(FONT_KEY_ROBOTO_CONDENSED_21));
-    text_layer_set_text_alignment(&text_layer, GTextAlignmentCenter);
-    layer_add_child(&window.layer, &text_layer.layer);
-
-    /*
-    ** Set time initially...
-    */
-    event.units_changed = HOUR_UNIT | MINUTE_UNIT | SECOND_UNIT;
-    event.tick_time = &now;
-    get_time(&now);
-    handle_tick(ctx, &event);
-}
-
-void handle_tick(AppContextRef ctx,
-		 PebbleTickEvent *event) {
-
-    static char text_string[] = "HHMMSS"; /* Filled out with maximum values
-					     for sizeof(). */
-    int ones, tens;
-
-    if (event->units_changed & DAY_UNIT) {
-    	/* do nothing, right now... */
+    window = window_create();
+    if (window == 0) {
+    	APP_LOG(APP_LOG_LEVEL_DEBUG, "Could not allocate window");
 	return;
-    } else {
-    	if (event->units_changed & HOUR_UNIT) {
-	    tens = event->tick_time->tm_hour / 10;
-    	    ones = event->tick_time->tm_hour % 10;
-
-	    if (!clock_is_24h_style() && (event->tick_time->tm_hour > 12)) {
-		tens--;
-		ones -= 2;
-	    }
-
-    	    bitmap_layer_set_bitmap(&bar_layer[0], &bar[tens].bmp);
-    	    bitmap_layer_set_bitmap(&bar_layer[1], &bar[ones].bmp);
-    	}
-        if (event->units_changed & MINUTE_UNIT) {
-	    tens = event->tick_time->tm_min / 10;
-    	    ones = event->tick_time->tm_min % 10;
-
-    	    bitmap_layer_set_bitmap(&bar_layer[2], &bar[tens].bmp);
-    	    bitmap_layer_set_bitmap(&bar_layer[3], &bar[ones].bmp);
-    	}
-   	if (event->units_changed & SECOND_UNIT) {
-	    tens = event->tick_time->tm_sec / 10;
-    	    ones = event->tick_time->tm_sec % 10;
-
-    	    bitmap_layer_set_bitmap(&bar_layer[4], &bar[tens].bmp);
-    	    bitmap_layer_set_bitmap(&bar_layer[5], &bar[ones].bmp);
-    	}
-
-	string_format_time(text_string, sizeof(text_string),
-			   (clock_is_24h_style() ? "%H%M%S" : "%I%M%S"),
-			   event->tick_time);
-	text_layer_set_text(&text_layer, text_string);
     }
+    window_layer = window_get_root_layer(window);
+
+    /*
+    ** Load graphics for each bar.
+    */
+    for (i = 0; i < sizeof(bar) / sizeof(bar[0]); i++)
+    	bar[i] = gbitmap_create_with_resource(BAR_RESOURCE_ID[i]);
+    left = gbitmap_create_with_resource(RESOURCE_ID_LEFT);
+    right = gbitmap_create_with_resource(RESOURCE_ID_RIGHT);
+
+    /*
+    ** Initialize the layers.
+    */
+    left_layer = bitmap_layer_create(GRect(21, 10, 6, 135));
+    bitmap_layer_set_bitmap(left_layer, left);
+    layer_add_child(window_layer, bitmap_layer_get_layer(left_layer));
+    for (i = 0; i < sizeof(bar_layer) / sizeof(bar_layer[0]); i++) {
+    	bar_layer[i] = bitmap_layer_create(GRect(27 + (i * 14), 10, 14, 120));
+	layer_add_child(window_layer, bitmap_layer_get_layer(bar_layer[i]));
+    }
+    right_layer = bitmap_layer_create(GRect(111, 10, 12, 135));
+    bitmap_layer_set_bitmap(right_layer, right);
+    layer_add_child(window_layer, bitmap_layer_get_layer(right_layer));
 }
 
-void handle_deinit(AppContextRef ctx) {
+static void deinit(void) {
 
     unsigned i;
 
     /*
-    ** Tidy up the markers.
+    ** Destroy all bitmaps.
     */
-    heap_bitmap_deinit(&left);
-    heap_bitmap_deinit(&right);
+    for (i = 0; i < sizeof(bar) / sizeof(bar[0]); i++)
+    	gbitmap_destroy(bar[i]);
+    gbitmap_destroy(left);
+    gbitmap_destroy(right);
 
     /*
-    ** Tidy up the bars.
+    ** Destroy all layers.
     */
-    for (i = 0; i < sizeof(bar)/sizeof(bar[0]); i++) {
-        heap_bitmap_deinit(&bar[i]);
-    }
+    for (i = 0; i < sizeof(bar_layer) / sizeof(bar_layer[0]); i++)
+    	bitmap_layer_destroy(bar_layer[i]);
+    bitmap_layer_destroy(left_layer);
+    bitmap_layer_destroy(right_layer);
 
-    return;
+    window_destroy(window);
+}
+
+int main(void) {
+    init();
+    app_event_loop();
+    deinit();
 }
